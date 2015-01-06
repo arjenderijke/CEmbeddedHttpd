52538173de3997b72d5479e0971545efb98f61ff
Arjen de Rijke <arjenderijke@gmail.com>
2015-01-06 10:13:06 +0100

    print headers

--- libqueryhandler/libqueryhandler.c
+++ libqueryhandler/libqueryhandler.c
@@ -12,12 +12,25 @@
 
 #define PORT 8888
 
-int
-answer_example1 (void *cls, struct MHD_Connection *connection,
-		 const char *url, const char *method,
-		 const char *version, const char *upload_data,
-		 size_t *upload_data_size, void **con_cls)
+static int
+print_out_key (void *cls, enum MHD_ValueKind kind, const char *key,
+               const char *value)
 {
+    printf ("%s: %s\n", key, value);
+    return MHD_YES;
+}
+
+int
+handle_request (void *cls, struct MHD_Connection *connection,
+		const char *url, const char *method,
+		const char *version, const char *upload_data,
+		size_t *upload_data_size, void **con_cls)
+{
+    printf ("New %s request for %s using version %s\n", method, url, version);
+
+    MHD_get_connection_values (connection, MHD_HEADER_KIND, print_out_key,
+			       NULL);
+
     const char *page = "<html><body>Hello, browser!</body></html>";
     struct MHD_Response *response;
     int ret;
@@ -35,7 +48,7 @@ void run_query_handler() {
     struct MHD_Daemon *daemon;
 
     daemon = MHD_start_daemon (MHD_USE_SELECT_INTERNALLY, PORT, NULL, NULL,
-				   &answer_example1, NULL, MHD_OPTION_END);
+			       &handle_request, NULL, MHD_OPTION_END);
     if (NULL == daemon)
 	return 1;
 
